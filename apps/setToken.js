ac5cced4b542abfa4ee4d3d6168ffb06cd16cbc4
SmallK111407 <3399280843@qq.com>
2024-05-02 11:58:23 +0800

    权限修改

--- apps/setToken.js
+++ apps/setToken.js
@@ -12,47 +12,55 @@ export class setToken extends plugin {
             rule: [
                 {
                     reg: "^#?(s|S)(p|P)(otify)?设置(cid|cse|atk)",
-                    fnc: "setting"
+                    fnc: "configSetting"
+                },
+                {
+                    reg: "^#?(s|S)(p|P)(otify)?设置atk",
+                    fnc: "tokenSetting"
                 }
             ]
         })
         this.jsonPath = `${pluginRoot}/data/userData/userData.json`
     }
 
-    async setting() {
+    async configSetting() {
         if (!this.e.isMaster) return false
-        const test = /^#?(s|S)(p|P)(otify)?设置(cid|cse|atk)(.*)/
+        const test = /^#?(s|S)(p|P)(otify)?设置(cid|cse)(.*)/
         const match = this.e.msg.match(test)
         if (match) {
             const key = match[4]
             const keyReplace = {
                 "cid": "clientID",
                 "cse": "clientSecret",
-                "atk": "accessToken"
             }
             const replacedKey = keyReplace[key]
             const matchedMsg = match[5]
-            if (!key === "atk") {
-                setting.setConfig("config", replacedKey, matchedMsg)
-            } else {
-                let data = []
-                if (fs.existsSync(this.jsonPath)) {
-                    data = JSON.parse(fs.readFileSync(this.jsonPath, "utf8"))
-                }
-                let found = false
-                for (let item of data) {
-                    if (item.userId === this.e.user_id) {
-                        item.token = matchedMsg
-                        found = true
-                        break
-                    }
-                }
-                if (!found) {
-                    data.push({ userId: this.e.user_id, token: matchedMsg })
-                }
-                fs.writeFileSync(this.jsonPath, JSON.stringify(data, null, 2), "utf8")
-            }
+            setting.setConfig("config", replacedKey, matchedMsg)
             await this.e.reply(`[Spotify插件]设置${replacedKey}成功!`, true)
+        }
+    }
+    async tokenSetting() {
+        const test = /^#?(s|S)(p|P)(otify)?设置atk(.*)/
+        const match = this.e.msg.match(test)
+        if (match) {
+            const matchedMsg = match[4]
+            let data = []
+            if (fs.existsSync(this.jsonPath)) {
+                data = JSON.parse(fs.readFileSync(this.jsonPath, "utf8"))
+            }
+            let found = false
+            for (let item of data) {
+                if (item.userId === this.e.user_id) {
+                    item.token = matchedMsg
+                    found = true
+                    break
+                }
+            }
+            if (!found) {
+                data.push({ userId: this.e.user_id, token: matchedMsg })
+            }
+            fs.writeFileSync(this.jsonPath, JSON.stringify(data, null, 2), "utf8")
+            await this.e.reply(`[Spotify插件]设置个人Access-Token成功!`, true)
         }
     }
 }
